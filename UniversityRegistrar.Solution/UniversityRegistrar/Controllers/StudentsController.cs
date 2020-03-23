5ec146af7dde656c485452f7d8287e8e6e2ab6fd
Jiwon <jiwon1.han@gmail.com>
2020-03-23 12:01:30 -0700

    Add student details

--- UniversityRegistrar.Solution/UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar.Solution/UniversityRegistrar/Controllers/StudentsController.cs
@@ -85,19 +85,19 @@ namespace UniversityRegistrar.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Delete(int id) 
-    // {
-    //   var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-    //   return View(thisCategory);
-    // }
+    public ActionResult Delete(int id) 
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-    //   _db.Categories.Remove(thisCategory);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      _db.Students.Remove(thisStudent);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
